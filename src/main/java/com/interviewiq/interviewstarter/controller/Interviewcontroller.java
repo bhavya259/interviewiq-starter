93d2976ef719a7d82b8455422f9184f2612207d5
Bhavya <bhavya@gmail.com>
2026-05-07 13:16:54 +0530

    interview api

---
+++ src/main/java/com/interviewiq/interviewstarter/controller/Interviewcontroller.java
@@ -0,0 +1,26 @@
+package com.interviewiq.interviewstarter.controller;
+
+import com.interviewiq.interviewstarter.dto.InterviewDtos.*;
+import com.interviewiq.interviewstarter.entity.Interview;
+import com.interviewiq.interviewstarter.service.InterviewService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/interview")
+public class Interviewcontroller {
+    private final InterviewService interviewService;
+
+    public Interviewcontroller(InterviewService interviewService){
+        this.interviewService = interviewService;
+    }
+
+    @PostMapping("/create")
+    public InterviewResponse create(@RequestBody InterviewRequest request){
+        Interview saved = interviewService.create(request.getRole(), request.getExperienceLevel(),request.getDifficulty(), request.getDuration());
+        return new InterviewResponse(true,"Interview created",saved.getId());
+    }
+
+}
---
+++ src/main/java/com/interviewiq/interviewstarter/service/InterviewService.java
@@ -0,0 +1,24 @@
+package com.interviewiq.interviewstarter.service;
+
+import com.interviewiq.interviewstarter.entity.Interview;
+import com.interviewiq.interviewstarter.repository.InterviewRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class InterviewService {
+
+    private final InterviewRepository interviewRepository;
+
+    public InterviewService(InterviewRepository interviewRepository){
+        this.interviewRepository=interviewRepository;
+    }
+
+    public Interview create(String role, String experienceLevel, String difficulty, Integer duration){
+        Interview interview = new Interview();
+        interview.setRole(role);
+        interview.setExperiencelevel(experienceLevel);
+        interview.setDifficulty(difficulty);
+        interview.setDuration(duration);
+        return interviewRepository.save(interview);
+    }
+}
